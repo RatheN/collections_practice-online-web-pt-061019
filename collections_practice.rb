495c76d9d5377f53a9166b7e7ea12a0671252981
Nathan Rathe <nprathe10@gmail.com>
2019-06-20 20:51:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,10 +11,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  temp_array = array
-  array[1] = temp_array[2]
-  array[2] = temp_array[1]
-  array
+  array[1], array[2] = array[2], array[1]
 end
 
 def reverse_array(array)
